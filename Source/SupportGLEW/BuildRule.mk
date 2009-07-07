ab6f2c7a89ffedc0fbb9996928270add1040ed1e
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2009-07-07 04:41:53 +0000

    Add a kvs::glew::ParticleVolumeRenderer class.

--- Source/SupportGLEW/BuildRule.mk
+++ Source/SupportGLEW/BuildRule.mk
@@ -3,6 +3,8 @@
 
 
 OBJECTS := \
+$(OUTDIR)/./Renderer/EnsembleAverageBuffer.o \
+$(OUTDIR)/./Renderer/ParticleVolumeRenderer.o \
 $(OUTDIR)/./Viewer/BufferObject.o \
 $(OUTDIR)/./Viewer/FragmentShader.o \
 $(OUTDIR)/./Viewer/FrameBufferObject.o \
@@ -23,9 +25,17 @@ $(OUTDIR)/./Viewer/%.o: ./Viewer/%.cpp ./Viewer/%.h
 	$(MKDIR) $(OUTDIR)/./Viewer
 	$(CPP) -c $(CPPFLAGS) $(DEFINITIONS) $(INCLUDE_PATH) -o $@ $<
 
+$(OUTDIR)/./Renderer/%.o: ./Renderer/%.cpp ./Renderer/%.h
+	$(MKDIR) $(OUTDIR)/./Renderer
+	$(CPP) -c $(CPPFLAGS) $(DEFINITIONS) $(INCLUDE_PATH) -o $@ $<
+
 
 install::
 	$(MKDIR) $(INSTALL_DIR)/include/SupportGLEW/.
 	$(INSTALL) ./*.h $(INSTALL_DIR)/include/SupportGLEW/.
+	$(MKDIR) $(INSTALL_DIR)/include/SupportGLEW/./Renderer
+	$(INSTALL) ./Renderer/*.h $(INSTALL_DIR)/include/SupportGLEW/./Renderer
+	$(MKDIR) $(INSTALL_DIR)/include/SupportGLEW/./Renderer/ParticleVolumeRenderer
+	$(INSTALL) ./Renderer/ParticleVolumeRenderer/*.h $(INSTALL_DIR)/include/SupportGLEW/./Renderer/ParticleVolumeRenderer
 	$(MKDIR) $(INSTALL_DIR)/include/SupportGLEW/./Viewer
 	$(INSTALL) ./Viewer/*.h $(INSTALL_DIR)/include/SupportGLEW/./Viewer
