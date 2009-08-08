a16d4da7875f3ca033167614c960efdf0935efda
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2009-08-08 14:17:54 +0000

    First import.

---
+++ Source/Core/Visualization/Event/EventBase.cpp
@@ -0,0 +1,39 @@
+/*****************************************************************************/
+/**
+ *  @file   EventBase.cpp
+ *  @author Naohisa Sakamoto
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright 2007 Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/*****************************************************************************/
+#include "EventBase.h"
+
+
+namespace kvs
+{
+
+/*===========================================================================*/
+/**
+ *  @brief  Constructs a new EventBase class.
+ */
+/*===========================================================================*/
+EventBase::EventBase( void )
+{
+}
+
+/*===========================================================================*/
+/**
+ *  @brief  Destructs the EventBase class.
+ */
+/*===========================================================================*/
+EventBase::~EventBase( void )
+{
+}
+
+} // end of namespace kvs
