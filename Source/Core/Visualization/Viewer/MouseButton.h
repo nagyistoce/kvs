3150984f00ecc8fc24f4db3e7da37df5f703bc85
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2009-08-08 15:08:56 +0000

    Add 'Action'.

--- Source/Core/Visualization/Viewer/MouseButton.h
+++ Source/Core/Visualization/Viewer/MouseButton.h
@@ -35,6 +35,16 @@ struct MouseButton
         Down = 0x00000000,
         Up   = 0x00000001
     };
+
+    enum Action
+    {
+        NoAction      = 0x00000000,
+        Pressed       = 0x00000001,
+        Moved         = 0x00000002,
+        Released      = 0x00000003,
+        DoubleClicked = 0x00000004,
+        Wheeled       = 0x00000005,
+    };
 };
 
 } // end of namespace kvs
