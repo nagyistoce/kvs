c2869171276f697a49963236346eb8e051f6fb13
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2009-08-24 10:23:34 +0000

    First import.

---
+++ Source/SupportQt/Viewer/Application.h
@@ -0,0 +1,56 @@
+/*****************************************************************************/
+/**
+ *  @file   Application.h
+ *  @author Naohisa Sakamoto
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright 2007 Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/*****************************************************************************/
+#ifndef KVS__QT__APPLICATION_H_INCLUDE
+#define KVS__QT__APPLICATION_H_INCLUDE
+
+#include <kvs/ApplicationBase>
+#include <kvs/qt/QT>
+
+
+namespace kvs
+{
+
+namespace qt
+{
+
+/*===========================================================================*/
+/**
+ *  @brief  Qt viewer application class.
+ */
+/*===========================================================================*/
+class Application : public kvs::ApplicationBase
+{
+private:
+
+    QApplication* m_app; ///< Qt application
+
+public:
+
+    Application( int argc, char** argv );
+
+    virtual ~Application( void );
+
+public:
+
+    virtual int run( void );
+
+    virtual void quit( void );
+};
+
+} // end of namespace qt
+
+} // end of namespace kvs
+
+#endif // KVS__QT__APPLICATION_H_INCLUDE
