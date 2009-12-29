6d58a2b51267d094d7c02388e7635d55e84bc895
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2009-12-29 02:43:55 +0000

    Modified.

--- trunk/Example/SupportQt/SimpleTriangle/main.cpp
+++ trunk/Example/SupportQt/SimpleTriangle/main.cpp
@@ -15,102 +15,66 @@
 /*****************************************************************************/
 // KVS
 #include <kvs/OpenGL>
-#include <kvs/Message>
-#include <kvs/RGBColor>
+#include <kvs/PaintEventListener>
 // SupportQt
-#include <kvs/qt/ScreenBase>
-#include <kvs/qt/GlobalBase>
+#include <kvs/qt/Application>
+#include <kvs/qt/Screen>
 // SupportGLUT
-#include <kvs/glut/Text>
+#include <kvs/glut/Label>
 
 
 /*===========================================================================*/
 /**
- *  @class  Global
- *  @brief  User-defined global parameter class.
+ *  @brief  User-defined paint event.
  */
 /*===========================================================================*/
-class Global : public kvs::qt::GlobalBase
+class PaintEvent : public kvs::PaintEventListener
 {
-public:
-
-    static kvs::glut::Text message; ///< text message
-
-    Global( int count, char** values ) :
-        kvs::qt::GlobalBase( count, values )
+    void update( void )
     {
-        // Message of the mouse manipulation.
-        message.addText( "Rotation:    [Left Button]" );
-        message.addText( "Translation: [Right Button] or [Ctrl] + [Left Button]" );
-        message.addText( "Scaling:     [Shift] + [Left Button] or [Wheel Scroll]" );
-        message.addText( "Quit:        [Esc] or [q]" );
-    }
-};
-
-// Instantiation.
-kvs::glut::Text Global::message;
-
-/*===========================================================================*/
-/**
- *  @class  Screen
- *  @brief  User-defined screen class.
- */
-/*===========================================================================*/
-class Screen : public kvs::qt::ScreenBase
-{
-public:
-
-    Screen( void )
-    {
-        addPaintEvent( additional_paint_event );
-    }
-
-    static void additional_paint_event( void )
-    {
+        /* This sample method draws a rainbow colored triangle in the screen by
+         * using GL_TRIANGLES.
+         */
         glBegin( GL_TRIANGLES );
         glColor3ub( 255,   0,   0 ); glVertex3d(  0.0,  3.0, 0.0 );
         glColor3ub(   0, 255,   0 ); glVertex3d(  3.0, -3.0, 0.0 );
         glColor3ub(   0,   0, 255 ); glVertex3d( -3.0, -3.0, 0.0 );
         glEnd();
-
-        const int x = 10;
-        const int y = 50;
-        Global::message.setPosition( x, y );
-        Global::message.draw();
     }
 };
 
 /*===========================================================================*/
 /**
  *  @brief  Main function.
- *  @param  argc [i] argument counter
+ *  @param  argc [i] argument count
  *  @param  argv [i] argument values
  */
 /*===========================================================================*/
 int main( int argc, char** argv )
 {
-    // Create the global parameters.
-    Global* global = new Global( argc, argv );
-    if( !global )
-    {
-        kvsMessageError("Cannot allocate memory for 'global'.");
-        return( false );
-    }
+    // Application.
+    kvs::qt::Application app( argc, argv );
 
-    // Create and show the rendering screen.
-    Screen* screen = new Screen();
-    if( !screen )
-    {
-        kvsMessageError("Cannot allocate memory for 'screen'.");
-        return( false );
-    }
-    screen->setGeometry( 50, 50, 512, 512 );
-    screen->setTitle( "SimpleTriangle" );
-    screen->show();
+    // User specified event.
+    PaintEvent paint_event;
 
-    // Delete the global parameters and the rendering screen.
-    delete global;
-    delete screen;
+    // Screen.
+    kvs::qt::Screen screen;
+    screen.addPaintEvent( &paint_event );
+    screen.setGeometry( 0, 0, 512, 512 );
+    screen.setTitle( "SimpleTriangle" );
+    screen.show();
 
-    return( true );
+    // Message.
+/*
+    kvs::glut::Label message( &screen );
+    message.setMargin( 10 );
+    message.addText( "Rotation:    [Left Button]" );
+    message.addText( "Translation: [Right Button] or [Ctrl] + [Left Button]" );
+    message.addText( "Scaling:     [Shift] + [Left Button]" );
+    message.addText( "Quit:        [Esc] or [q]" );
+    message.show();
+*/
+
+    return( app.run() );
 }
