528e65d72dbd540b13ed38d7b510c30a27bd3082
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2010-01-12 12:02:05 +0000

    Add some properties in the CUDA device class.

--- Example/SupportCUDA/GPUInformation/main.cpp
+++ Example/SupportCUDA/GPUInformation/main.cpp
@@ -34,10 +34,21 @@ int main( int argc, char** argv )
     for ( size_t i = 0; i < ngpus; i++ )
     {
         kvs::cuda::Device device( i );
-        std::cout << "ID:           " << i << std::endl;
-        std::cout << "Name:         " << device.name() << std::endl;
+        std::cout << "ID: " << i << std::endl;
+        std::cout << "Name: " << device.name() << std::endl;
+        std::cout << "Compute capability: " << device.majorRevision() << "." << device.minorRevision() << std::endl;
         std::cout << "Total memory: " << device.totalMemory() / 1024 / 1024 << " [MB]" << std::endl;
-        std::cout << "Free memory:  " << device.freeMemory() / 1024 / 1024 << " [MB]" << std::endl;
+        std::cout << "Free memory: " << device.freeMemory() / 1024 / 1024 << " [MB]" << std::endl;
+        std::cout << "Max. number of threads per block: " << device.maxThreadsPerBlock() << std::endl;
+        std::cout << "Max. size of each dimension of a block: " << device.maxThreadsDimension() << std::endl;
+        std::cout << "Max. size of each dimension of a grid: " << device.maxGridSize() << std::endl;
+        std::cout << "Shared memory per block: " << device.sharedMemoryPerBlock() / 1024 << " [KB]" << std::endl;
+        std::cout << "Total amount of constant memory: " << device.totalConstantMemory() / 1024 << " [KB]" << std::endl;
+        std::cout << "Warp size: " << device.warpSize() << " [B]" << std::endl;
+        std::cout << "Memory pitch: " << device.memoryPitch() / 1024 << " [KB]" << std::endl;
+        std::cout << "Number of registers per block: " << device.registersPerBlock() << std::endl;
+        std::cout << "Clock rate: " << device.clockRate() / 1000 << " [MHz]" << std::endl;
+        std::cout << "Texture alignment: " << device.textureAlignment() / 1024 << " [MB]" << std::endl;
     }
 
     return 0;
