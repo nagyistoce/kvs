e7ec5b224e42085cb04e227d8b813e859dee87ce
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2010-04-28 11:29:36 +0000

    Fixed a problem.

--- trunk/Tool/kvscheck/Argument.h
+++ trunk/Tool/kvscheck/Argument.h
@@ -30,22 +30,31 @@ class Argument : public kvs::CommandLine
 {
 public:
 
-    Argument( int argc, char** argv ):
-        kvs::CommandLine( argc, argv, KVSCHECK_COMMAND_NAME )
-    {
-        add_help_option();
-        add_option("version", "Output KVS version. (optional)", 0 );
-        add_option("platform", "Output platforma information. (optional)", 0 );
-        add_option("compiler", "Output compiler information. (optional)", 0 );
-        add_option("sizeof", "Output 'sizeof' information. (optional)", 0 );
-        add_option("support", "Output supported library information. (optional)", 0 );
-        add_option("minmax", "Output min/max information. (optional)", 0 );
-        add_option("opengl", "Output OpenGL information. (optional)", 0 );
-        add_option("extension", "Output OpenGL extension information. (optional)", 0 );
-        add_option("file", "Output file information. (optional)", 0 );
-        add_value("input value");
-    };
+    Argument( int argc, char** argv );
 };
+
+/*===========================================================================*/
+/**
+ *  @brief  Constructs a new Argument class.
+ *  @param  argc [in] argument count
+ *  @param  argv [in] argument values
+ */
+/*===========================================================================*/
+Argument::Argument( int argc, char** argv ):
+    kvs::CommandLine( argc, argv, KVSCHECK_COMMAND_NAME )
+{
+    add_help_option();
+    add_option("version", "Output KVS version. (optional)");
+    add_option("platform", "Output platforma information. (optional)");
+    add_option("compiler", "Output compiler information. (optional)");
+    add_option("sizeof", "Output 'sizeof' information. (optional)");
+    add_option("support", "Output supported library information. (optional)");
+    add_option("minmax", "Output min/max information. (optional)");
+    add_option("opengl", "Output OpenGL information. (optional)");
+    add_option("extension", "Output OpenGL extension information. (optional)");
+    add_option("file", "Output file information. (optional)");
+    add_value("input value", false);
+}
 
 } // end of namespace kvscheck
 
