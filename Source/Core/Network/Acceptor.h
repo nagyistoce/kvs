47f64126f2ce47b47ed585ba41598fc372346ae1
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2010-04-28 11:26:51 +0000

    First import.

---
+++ Source/Core/Network/Acceptor.h
@@ -0,0 +1,65 @@
+/*****************************************************************************/
+/**
+ *  @file   Acceptor.h
+ *  @author Naohisa Sakamoto
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright 2007 Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/*****************************************************************************/
+#ifndef KVS__ACCEPTOR_H_INCLUDE
+#define KVS__ACCEPTOR_H_INCLUDE
+
+#include <kvs/TCPServer>
+#include <kvs/IPAddress>
+
+
+namespace kvs
+{
+
+/*===========================================================================*/
+/**
+ *  @brief  Acceptor class.
+ */
+/*===========================================================================*/
+class Acceptor
+{
+private:
+
+    kvs::TCPServer* m_handler; //< handler
+
+public:
+
+    Acceptor( void );
+
+    Acceptor( const int port, const size_t ntrials );
+
+    ~Acceptor( void );
+
+public:
+
+    const bool open( void );
+
+    void close( void );
+
+    const bool bind( const int port, const size_t ntrials );
+
+    kvs::TCPSocket* newConnection( void );
+
+    const int receive( kvs::MessageBlock* block, kvs::SocketAddress* client_address = 0 );
+
+private:
+
+    const bool create_handler( void );
+
+    void delete_handler( void );
+};
+
+} // end of namespace kvs
+
+#endif // KVS__ACCEPTOR_H_INCLUDE
