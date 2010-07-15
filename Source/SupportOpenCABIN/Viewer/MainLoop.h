feeb20edfa591be3e64cb09e6743eda6712e663d
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2010-07-15 06:12:15 +0000

    First import.

---
+++ Source/SupportOpenCABIN/Viewer/MainLoop.h
@@ -0,0 +1,52 @@
+/*****************************************************************************/
+/**
+ *  @file   MainLoop.h
+ *  @author Naohisa Sakamoto
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright 2007 Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/*****************************************************************************/
+#ifndef KVS__OPENCABIN__MAIN_LOOP_H_INCLUDE
+#define KVS__OPENCABIN__MAIN_LOOP_H_INCLUDE
+
+#include <kvs/Thread>
+
+
+namespace kvs
+{
+
+namespace opencabin
+{
+
+/*===========================================================================*/
+/**
+ *  @brief  Main loop class for OpenCABIN.
+ */
+/*===========================================================================*/
+class MainLoop : public kvs::Thread
+{
+    typedef kvs::Thread BaseClass;
+
+protected:
+
+    int    m_argc; ///< argument count
+    char** m_argv; ///< argument values
+
+public:
+
+    MainLoop( int argc, char** argv );
+
+    void run( void );
+};
+
+} // end of namespace opencabin
+
+} // end of namespace kvs
+
+#endif // KVS__OPENCABIN__MAIN_LOOP_H_INCLUDE
