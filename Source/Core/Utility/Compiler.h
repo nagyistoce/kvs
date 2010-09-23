6b35a52d132e95a4194bd95f606595711e467bf5
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2010-09-23 13:42:20 +0000

    Modified for VC2010.

--- Source/Core/Utility/Compiler.h
+++ Source/Core/Utility/Compiler.h
@@ -108,6 +108,18 @@
 #define KVS_COMPILER_VERSION_GREATER_OR_EQUAL( major, minor ) \
     ( 9 > ( major ) || ( 9 == ( major ) && 0 >= ( minor ) ) )
 
+#elif ( _MSC_VER == 1600 )
+#define KVS_COMPILER_VERSION "10.00"
+#define KVS_COMPILER_VERSION_IS( major, minor ) ( 10 == ( major ) && 0 == ( minor ) )
+#define KVS_COMPILER_VERSION_LESS_THAN( major, minor ) \
+    ( 10 < ( major ) || ( 10 == ( major ) && 0 < ( minor ) ) )
+#define KVS_COMPILER_VERSION_LESS_OR_EQUAL( major, minor ) \
+    ( 10 < ( major ) || ( 10 == ( major ) && 0 <= ( minor ) ) )
+#define KVS_COMPILER_VERSION_GREATER_THAN( major, minor ) \
+    ( 10 > ( major ) || ( 10 == ( major ) && 0 > ( minor ) ) )
+#define KVS_COMPILER_VERSION_GREATER_OR_EQUAL( major, minor ) \
+    ( 10 > ( major ) || ( 10 == ( major ) && 0 >= ( minor ) ) )
+
 #else
 #define KVS_COMPILER_VERSION "unknown"
 #define KVS_COMPILER_VERSION_IS( major, minor )
