ee27cfa7c120d13caa1b1c25780dfdc8c134ad92
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2010-10-01 07:30:23 +0000

    Rename.

---
+++ Tool/kvscheck/CommandName.h
@@ -0,0 +1,27 @@
+/****************************************************************************/
+/**
+ *  @file kvscheckMacro.h
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright (c) Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/****************************************************************************/
+#ifndef KVSCHECK__KVSCHECK_MACRO_H_INCLUDE
+#define KVSCHECK__KVSCHECK_MACRO_H_INCLUDE
+
+#include <string>
+
+
+namespace kvscheck
+{
+
+const std::string KVSCHECK_COMMAND_NAME = "kvscheck";
+
+}
+
+#endif // KVSCHECK__KVSCHECK_MACRO_H_INCLUDE
