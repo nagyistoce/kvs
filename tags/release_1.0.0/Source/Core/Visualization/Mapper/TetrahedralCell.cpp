b1c2990d471f0e6738e447e6d08ba108533a17a1
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2010-10-17 12:07:42 +0000

    1.0.0 released

---
+++ tags/release_1.0.0/Source/Core/Visualization/Mapper/TetrahedralCell.cpp
@@ -0,0 +1,49 @@
+/****************************************************************************/
+/**
+ *  @file TetrahedralCell.cpp
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright (c) Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/****************************************************************************/
+#include "TetrahedralCell.h"
+
+
+namespace kvs
+{
+
+// template instantiation
+template class TetrahedralCell<char>;
+template class TetrahedralCell<unsigned char>;
+template class TetrahedralCell<short>;
+template class TetrahedralCell<unsigned short>;
+template class TetrahedralCell<int>;
+template class TetrahedralCell<unsigned int>;
+template class TetrahedralCell<long>;
+template class TetrahedralCell<unsigned long>;
+template class TetrahedralCell<float>;
+template class TetrahedralCell<double>;
+
+namespace old
+{
+
+// template instantiation
+template class TetrahedralCell<char>;
+template class TetrahedralCell<unsigned char>;
+template class TetrahedralCell<short>;
+template class TetrahedralCell<unsigned short>;
+template class TetrahedralCell<int>;
+template class TetrahedralCell<unsigned int>;
+template class TetrahedralCell<long>;
+template class TetrahedralCell<unsigned long>;
+template class TetrahedralCell<float>;
+template class TetrahedralCell<double>;
+
+} // end of namespace old
+
+} // end of namespace kvs
