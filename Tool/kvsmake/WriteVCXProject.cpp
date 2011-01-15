e259fd656b01152da81b60fd8809d078143374b2
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2011-01-15 03:55:08 +0000

    First import.

---
+++ Tool/kvsmake/WriteVCXProject.cpp
@@ -0,0 +1,94 @@
+/****************************************************************************/
+/**
+ *  @file WriteVCProject.cpp
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright (c) Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/****************************************************************************/
+#include "WriteVCXProject.h"
+
+#include <string>  // For std::string.
+#include <fstream> // For std::ifstream, std::ofstream.
+
+#include <kvs/Message>
+#include <kvs/Directory>
+#include <kvs/FileList>
+#include <kvs/File>
+
+#include "Constant.h"
+#include "ReplaceString.h"
+
+
+namespace kvsmake
+{
+
+void WriteVCXProject( const std::string& project_name )
+{
+    //  Open a template file.
+    std::ifstream in( kvsmake::VCXProjectTemplate.c_str() );
+    if ( !in.is_open() )
+    {
+        kvsMessageError( "Cannot open %s.", kvsmake::VCXProjectTemplate.c_str() );
+        return;
+    }
+
+    //  Open a project file.
+    const std::string filename( project_name + ".vcxproj" );
+
+    std::ofstream out( filename.c_str() );
+    if ( !out.is_open() )
+    {
+        kvsMessageError( "Cannot open %s.", filename.c_str() );
+        return;
+    }
+
+    kvsmake::WriteVCXProjectBody( in, out, project_name );
+}
+
+void WriteVCXProjectBody(
+    std::ifstream&     in,
+    std::ofstream&     out,
+    const std::string& project_name )
+{
+    //  Search the project's condition.
+    std::string includes( "" );
+
+    // Search cpp files and h files.
+    const kvs::Directory current_dir( "." );
+    const kvs::FileList& file_list = current_dir.fileList();
+
+    kvs::FileList::const_iterator       iter = file_list.begin();
+    const kvs::FileList::const_iterator end  = file_list.end();
+
+    while ( iter != end )
+    {
+        if ( ( iter->extension() == "h" ) || ( iter->extension() == "cpp" ) )
+        {
+            includes += ( "\t<ItemGroup>\n" );
+            includes += ( "\t\t<ClCompile Include=\"" + iter->fileName() + "\"/>\n" );
+            includes += ( "\t</ItemGroup>\n" );
+        }
+        ++iter;
+    }
+
+    // Write a project file.
+    while ( !in.eof() )
+    {
+        std::string line( "" );
+
+        std::getline( in, line );
+
+        line = kvsmake::ReplaceString( line, "PROJECT_NAME_REPLACED_BY_KVSMAKE", project_name );
+        line = kvsmake::ReplaceString( line, "INCLUDES_REPLACED_BY_KVSMAKE", includes );
+
+        out << line << std::endl;
+    }
+}
+
+} // end of namespace kvsmake
