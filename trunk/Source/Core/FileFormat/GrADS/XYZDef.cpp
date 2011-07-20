7fa8db380a36a09d465c0afbb6ef57ac18f6803a
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2011-07-20 10:40:29 +0000

    First import.

---
+++ trunk/Source/Core/FileFormat/GrADS/XYZDef.cpp
@@ -0,0 +1,89 @@
+/*****************************************************************************/
+/**
+ *  @file   XYZDef.cpp
+ *  @author Naohisa Sakamoto
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright (c) Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/*****************************************************************************/
+#include "XYZDef.h"
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <kvs/Tokenizer>
+
+
+namespace kvs
+{
+
+namespace grads
+{
+
+/*===========================================================================*/
+/**
+ *  @brief  Reads XDEF/YDEF/ZDEF entry.
+ *  @param  line [in] read line
+ *  @param  ifs [in] input file stream
+ *  @return true, if the reading process is done successfully
+ */
+/*===========================================================================*/
+const bool XYZDef::read( std::string line, std::ifstream& ifs )
+{
+    kvs::Tokenizer t( line, " \t\n" );
+    t.token(); // XDEF, YDEF or ZDEF
+
+    // xnum
+    if ( t.isLast() ) return( false );
+    this->num = static_cast<size_t>( atoi( t.token().c_str() ) );
+
+    // mapping
+    if ( t.isLast() ) return( false );
+    const std::string mapping = t.token();
+    if ( mapping == "LINEAR" || mapping == "linear" )
+    {
+        this->mapping = Linear;
+        this->values.allocate( 2 );
+        this->values[0] = static_cast<kvs::Real32>( atof( t.token().c_str() ) );
+        this->values[1] = static_cast<kvs::Real32>( atof( t.token().c_str() ) );
+    }
+    else if ( mapping == "LEVELS" || mapping == "levels" )
+    {
+        this->mapping = Levels;
+        this->values.allocate( this->num );
+
+        if ( !t.isLast() )
+        {
+            for ( size_t i = 0; i < this->num; i++ )
+            {
+                if ( t.isLast() ) return( false );
+                this->values[i] = static_cast<kvs::Real32>( atof( t.token().c_str() ) );
+            }
+        }
+        else
+        {
+            size_t counter = 0;
+            while ( std::getline( ifs, line ) )
+            {
+                t = kvs::Tokenizer( line, " \t" );
+                while ( !t.isLast() )
+                {
+                    this->values[counter++] = static_cast<kvs::Real32>( atof( t.token().c_str() ) );
+                    if ( counter >= this->num ) break;
+                }
+                if ( counter >= this->num ) break;
+            }
+        }
+    }
+
+    return( true );
+}
+
+} // end of namespace grads
+
+} // end of namespace kvs
