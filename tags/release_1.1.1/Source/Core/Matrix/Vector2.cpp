df4c5ce4162fb7b4a2ed2c82409c3721356ae295
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2011-12-09 09:20:08 +0000

    1.1.1 released

---
+++ tags/release_1.1.1/Source/Core/Matrix/Vector2.cpp
@@ -0,0 +1,32 @@
+/****************************************************************************/
+/**
+ *  @file Vector2.cpp
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright (c) Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/****************************************************************************/
+#include "Vector2.h"
+
+
+namespace kvs
+{
+
+// Template instantiation.
+template class Vector2<char>;
+template class Vector2<unsigned char>;
+template class Vector2<short>;
+template class Vector2<unsigned short>;
+template class Vector2<int>;
+template class Vector2<unsigned int>;
+template class Vector2<long>;
+template class Vector2<unsigned long>;
+template class Vector2<float>;
+template class Vector2<double>;
+
+} // end of namespace kvs
