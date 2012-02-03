065b447e296834c7e47bee6336eafba0cfdc73f1
naohisa.sakamoto@gmail.com <naohisa.sakamoto@gmail.com@23c4efe2-019b-11de-a295-0b7b0935bf94>
2012-02-03 12:12:52 +0000

    Fixed a problem.

--- Source/Core/Visualization/Mapper/GlyphBase.cpp
+++ Source/Core/Visualization/Mapper/GlyphBase.cpp
@@ -193,9 +193,9 @@ void GlyphBase::calculate_uniform_coords( const kvs::StructuredVolumeObject* vol
     const kvs::Vector3f  volume_size( volume->maxExternalCoord() - volume->minExternalCoord() );
     const kvs::Vector3ui ngrids( resolution - kvs::Vector3ui( 1, 1, 1 ) );
     const kvs::Vector3f  grid_size(
-        volume_size.x() / static_cast<float>( ngrids.x() ),
-        volume_size.y() / static_cast<float>( ngrids.y() ),
-        volume_size.z() / static_cast<float>( ngrids.z() ) );
+        ( ngrids.x() == 0 ) ? 0.0f : volume_size.x() / static_cast<float>( ngrids.x() ),
+        ( ngrids.y() == 0 ) ? 0.0f : volume_size.y() / static_cast<float>( ngrids.y() ),
+        ( ngrids.z() == 0 ) ? 0.0f : volume_size.z() / static_cast<float>( ngrids.z() ) );
 
     for ( size_t k = 0; k < resolution.z(); ++k )
     {
