6574cf17c38f4569f81b281bf4307b704b37041d
naohisa.sakamoto@gmail.com <naohisa.sakamoto@gmail.com@23c4efe2-019b-11de-a295-0b7b0935bf94>
2012-02-17 16:17:48 +0000

    Add StochasticMultivariateRenderer/Engine.

---
+++ trunk/Example/SupportGLEW/StochasticMultivariateTetrahedraRenderer/main.cpp
@@ -0,0 +1,54 @@
+/*****************************************************************************/
+/**
+ *  @file   main.cpp
+ *  @author Naohisa Sakamoto
+ *  @brief  Example program for kvs::glew::StochasticMultivariateTetrahedraRenderer class.
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright (c) Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/*****************************************************************************/
+#include <kvs/glut/Application>
+#include <kvs/glut/Screen>
+#include <kvs/UnstructuredVolumeObject>
+#include <kvs/UnstructuredVolumeImporter>
+#include <kvs/RGBFormulae>
+#include <kvs/TransferFunction>
+#include <kvs/glew/StochasticMultivariateTetrahedraRenderer>
+
+
+/*===========================================================================*/
+/**
+ *  @brief  Main function.
+ *  @param  argc [i] argument count
+ *  @param  argv [i] argument values
+ */
+/*===========================================================================*/
+int main( int argc, char** argv )
+{
+    kvs::glut::Application app( argc, argv );
+
+    kvs::UnstructuredVolumeObject* object = new kvs::UnstructuredVolumeImporter( argv[1] );
+
+    kvs::glew::StochasticMultivariateTetrahedraRenderer* renderer = new kvs::glew::StochasticMultivariateTetrahedraRenderer();
+    renderer->setRepetitionLevel( 50 );
+    renderer->enableLODControl( 1 );
+
+    kvs::TransferFunction tfunc;
+    tfunc.setColorMap( kvs::RGBFormulae::PM3D( 256 ) );
+    renderer->setTransferFunction( tfunc, 1 );
+
+    kvs::glut::Screen screen( &app );
+    screen.registerObject( object, renderer );
+    screen.show();
+
+    // Enable two-side (frontface and backface) lighting
+    screen.light()->setModelTwoSide( true );
+
+    return app.run();
+}
