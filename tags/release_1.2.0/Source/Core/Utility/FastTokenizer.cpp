4dd6ff606fda1ecce591a2c67e03cfef201a9672
naohisa.sakamoto <naohisa.sakamoto@23c4efe2-019b-11de-a295-0b7b0935bf94>
2012-04-09 09:04:27 +0000

    1.2.0 released

---
+++ tags/release_1.2.0/Source/Core/Utility/FastTokenizer.cpp
@@ -0,0 +1,50 @@
+/****************************************************************************/
+/**
+ *  @file FastTokenizer.cpp
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright (c) Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/****************************************************************************/
+#include "FastTokenizer.h"
+
+
+namespace kvs
+{
+
+/*==========================================================================*/
+/**
+ *  Constructs a new empty FastTokenizer.
+ */
+/*==========================================================================*/
+FastTokenizer::FastTokenizer( void )
+    : m_ntokens( 0 )
+{
+}
+
+/*==========================================================================*/
+/**
+ *  Constructs a new FastTokenizer.
+ */
+/*==========================================================================*/
+FastTokenizer::FastTokenizer( char* const source, const char* const delimiter )
+    : m_ntokens( 0 )
+{
+    this->set( source, delimiter );
+}
+
+/*==========================================================================*/
+/**
+ *  Destroys the FastTokenizer.
+ */
+/*==========================================================================*/
+FastTokenizer::~FastTokenizer( void )
+{
+}
+
+} // end of namespace test
---
+++ tags/release_1.2.0/Source/SupportGLEW/Renderer/StochasticRenderingEngine/line.frag
@@ -0,0 +1,50 @@
+/*****************************************************************************/
+/**
+ *  @file   line.frag
+ *  @author Jun Nishimura
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright (c) Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/*****************************************************************************/
+varying vec3 position;
+varying vec2 id;
+
+#if defined( ENABLE_EXACT_DEPTH_TESTING )
+varying float depth;
+#endif
+
+uniform sampler2D random_texture;
+
+uniform vec2 screen_scale;
+uniform vec2 screen_scale_inv;
+
+uniform float random_texture_size_inv;
+uniform vec2 random_offset;
+
+uniform float opacity;
+
+void main( void )
+{
+    if ( opacity == 0.0 ) { discard; return; }
+
+    if ( opacity < 0.99999 )
+    {
+        vec2 random_position = ( vec2( float( int( id.x ) * 73 ), float( int( id.y ) * 31 ) ) 
+                    + random_offset + gl_FragCoord.xy ) * random_texture_size_inv;
+
+        float randf = texture2D( random_texture, random_position ).a;
+        if ( randf > opacity ) { discard; return; }
+    }
+
+    gl_FragColor = vec4( gl_Color.rgb, 1.0 );
+
+#if defined( ENABLE_EXACT_DEPTH_TESTING )
+    gl_FragDepth = depth;
+#endif
+}
