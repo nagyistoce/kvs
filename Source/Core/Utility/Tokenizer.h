4adda026dfab9b26ed361d3945cb0440affb4d6e
s.yamada0808@gmail.com <s.yamada0808@gmail.com@23c4efe2-019b-11de-a295-0b7b0935bf94>
2012-05-07 15:11:27 +0000

    Modified.

--- Source/Core/Utility/Tokenizer.h
+++ Source/Core/Utility/Tokenizer.h
@@ -15,7 +15,6 @@
 #define KVS__TOKENIZER_H_INCLUDE
 
 #include <string>
-#include <kvs/ClassName>
 
 
 namespace kvs
@@ -28,39 +27,21 @@ namespace kvs
 /*==========================================================================*/
 class Tokenizer
 {
-    kvsClassName_without_virtual( kvs::Tokenizer );
-
-protected:
-
+private:
     std::string            m_source;    ///< source std::string
     std::string            m_delimiter; ///< delimiter set
-    std::string            m_token;     ///< token
-    std::string::size_type m_first;     ///< first position
-    std::string::size_type m_last;      ///< last position
+    std::string::size_type m_next;      ///< next token position
 
 public:
-
     Tokenizer();
-
     Tokenizer( const std::string& source, const std::string& delimiter );
 
-    Tokenizer( const Tokenizer& t );
-
 public:
-
-    Tokenizer& operator = ( const Tokenizer& t );
-
     Tokenizer& operator = ( const std::string& source );
 
 public:
-
     bool isLast() const;
-
     std::string token();
-
-protected:
-
-    void get_token();
 };
 
 } // end of namespace kvs
