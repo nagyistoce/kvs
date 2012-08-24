340ad0becc88ac2839a4ec6374bc1811ffc5829a
s.yamada0808@gmail.com <s.yamada0808@gmail.com@23c4efe2-019b-11de-a295-0b7b0935bf94>
2012-08-24 15:21:00 +0000

    Modified.

--- trunk/Source/Core/Visualization/Viewer/RendererManager.h
+++ trunk/Source/Core/Visualization/Viewer/RendererManager.h
@@ -15,14 +15,21 @@
 #define KVS__RENDERER_MANAGER_H_INCLUDE
 
 #include <string>
-#include <list>
+#include <utility>
+#include <vector>
+#if KVS_ENABLE_DEPRECATED
 #include <map>
+#include <list>
 #include <kvs/ClassName>
 #include <kvs/RendererBase>
+#endif
+#include <kvs/SharedPointer>
 
 
 namespace kvs
 {
+
+class RendererBase;
 
 /*==========================================================================*/
 /**
@@ -31,46 +38,59 @@ namespace kvs
 /*==========================================================================*/
 class RendererManager
 {
+#if KVS_ENABLE_DEPRECATED
     kvsClassName( kvs::RendererManager );
-
-public:
-    typedef std::list<kvs::RendererBase*>  RendererList;
-    typedef RendererList::iterator         RendererIterator;
-    typedef std::map<int,RendererIterator> RendererMap;
+#endif
 
 private:
-    RendererMap m_renderer_map; ///< renderer map
+    typedef kvs::SharedPointer<kvs::RendererBase>    RendererSP;
+    typedef std::vector<std::pair<int, RendererSP> > RendererList;
+    typedef RendererList::iterator                   Iterator;
+
+private:
     RendererList m_renderer_list;
+    int m_renderer_id;
 
 public:
-
-    RendererManager( void );
-
-    virtual ~RendererManager( void );
+    RendererManager();
+    virtual ~RendererManager();
+    //~RendererManager();
 
 public:
-
     int insert( kvs::RendererBase* renderer );
 
-    void erase( bool delete_flg = true );
+    void erase( bool delete_flg );
+    void erase( int renderer_id, bool delete_flg );
+    void erase( const std::string& renderer_name, bool delete_flg );
 
-    void erase( int renderer_id, bool delete_flg = true );
+    void change( int renderer_id, kvs::RendererBase* renderer, bool delete_flg );
+    void change( const std::string& renderer_name, kvs::RendererBase* renderer, bool delete_flg );
 
-    void erase( std::string renderer_name, bool delete_flg = true );
+    int nrenderers() const;
 
-    void change( int renderer_id, kvs::RendererBase* renderer, bool delete_flg = true );
-
-    void change( std::string renderer_name, kvs::RendererBase* renderer, bool delete_flg = true );
-
-    const int nrenderers( void ) const;
-
-    kvs::RendererBase* renderer( void );
-
+    kvs::RendererBase* renderer();
     kvs::RendererBase* renderer( int renderer_id );
+    kvs::RendererBase* renderer( const std::string& renderer_name );
 
-    kvs::RendererBase* renderer( std::string renderer_name );
+public:
+    int insert( const kvs::SharedPointer<kvs::RendererBase>& renderer );
 
-    const bool hasRenderer( void ) const;
+    void erase();
+    void erase( int renderer_id );
+    void erase( const std::string& renderer_name );
+
+    void change( int renderer_id, const kvs::SharedPointer<kvs::RendererBase>& renderer );
+    void change( const std::string& renderer_name, const kvs::SharedPointer<kvs::RendererBase>& renderer );
+
+    int numberOfRenderers() const;
+    bool hasRenderer() const;
+
+    //const kvs::SharedPointer<kvs::RendererBase>& renderer( int renderer_id );
+    //const kvs::SharedPointer<kvs::RendererBase>& renderer( const std::string& renderer_name );
+
+private:
+    Iterator find_renderer( int id );
+    Iterator find_renderer( const std::string& name );
 
 private:
     RendererManager( const RendererManager& );
