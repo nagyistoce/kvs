3aa06303576c82ae76e7e145ee06b7fe04faed13
naohisa.sakamoto@gmail.com <naohisa.sakamoto@gmail.com@23c4efe2-019b-11de-a295-0b7b0935bf94>
2013-03-16 09:38:22 +0000

    First import.

---
+++ trunk/Source/Core/OpenGL/GL.h
@@ -0,0 +1,83 @@
+/*****************************************************************************/
+/**
+ *  @file   GL.h
+ *  @author Naohisa Sakamoto
+ */
+/*----------------------------------------------------------------------------
+ *
+ *  Copyright (c) Visualization Laboratory, Kyoto University.
+ *  All rights reserved.
+ *  See http://www.viz.media.kyoto-u.ac.jp/kvs/copyright/ for details.
+ *
+ *  $Id$
+ */
+/*****************************************************************************/
+#ifndef KVS_GL_H_INCLUDE
+#define KVS_GL_H_INCLUDE
+
+#include <kvs/Macro>
+#include <kvs/Breakpoint>
+#include <kvs/Platform>
+
+#if !defined( KVS_PLATFORM_WINDOWS )
+#define GL_GLEXT_PROTOTYPES
+#endif
+
+#if defined( KVS_PLATFORM_WINDOWS )
+#include <cstdlib>
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#define GLUT_DISABLE_ATEXIT_HACK
+#endif
+
+/* NOTE: GLEW header file 'glew.h' must be included before the OpenGL header files.
+ * The 'glew.h' is formally included in SupportGLEW/GLEW.h. However, the 'glew.h'
+ * is included in this header file when 'KVS_SUPPORT_GLEW' is defined since some
+ * KVS classes that is compiled into the KVS core library uses the OpenGL functions
+ * by including this header file.
+ */
+#if defined( KVS_SUPPORT_GLEW )
+#include <GL/glew.h>
+#endif
+
+#if defined( KVS_PLATFORM_MACOSX )
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
+
+
+namespace kvs
+{
+
+namespace GL
+{
+
+namespace detail
+{
+
+bool HasError( const char* file, const int line, const char* func, const char* command );
+
+} // end of namespace detail
+
+} // end of namespace GL
+
+} // end of namespace kvs
+
+
+#if defined KVS_ENABLE_DEBUG
+#define KVS_GL_CALL( command )                                          \
+    KVS_MACRO_MULTI_STATEMENT_BEGIN                                     \
+    if ( kvs::GL::detail::HasError( KVS_MACRO_FILE, KVS_MACRO_LINE, KVS_MACRO_FUNC, "Unknown" ) ) { KVS_BREAKPOINT; } \
+    command;                                                            \
+    if ( kvs::GL::detail::HasError( KVS_MACRO_FILE, KVS_MACRO_LINE, KVS_MACRO_FUNC, #command ) ) { KVS_BREAKPOINT; } \
+    KVS_END_MULTI_LINE_MACRO_END
+#else
+#define KVS_GL_CALL( command ) ( command )
+#endif
+
+#endif // KVS_GL_H_INCLUDE
