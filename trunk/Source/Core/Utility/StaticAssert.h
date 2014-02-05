6d69943bf784f3b922b3e1f1be1e8dc9a44a4dc8
naohisa.sakamoto@gmail.com <naohisa.sakamoto@gmail.com@23c4efe2-019b-11de-a295-0b7b0935bf94>
2014-02-05 13:12:54 +0000

    Fixed a problem.

--- trunk/Source/Core/Utility/StaticAssert.h
+++ trunk/Source/Core/Utility/StaticAssert.h
@@ -3,8 +3,12 @@
 
 
 #if defined KVS_COMPILER_GCC
-  #if KVS_COMPILER_VERSION_LESS_THAN( 4, 8 )
+  #if KVS_COMPILER_VERSION_LESS_THAN( 3, 1 )
     #define KVS_STATIC_ASSERT( expr, mes ) typedef char KVS_STATIC_ASSERTION_FAILURE[ ( expr ) ? 1 : -1 ]
+  #elif KVS_COMPILER_VERSION_LESS_THAN( 4, 8 )
+    #define KVS_STATIC_ASSERT( expr, mes ) typedef char KVS_STATIC_ASSERTION_FAILURE[ ( expr ) ? 1 : -1 ] __attribute__((unused))
+  #else
+    #define KVS_STATIC_ASSERT( expr, mes ) static_assert( expr, mes )
   #endif
 #elif defined KVS_COMPILER_VC
   #if KVS_COMPILER_VERSION_LESS_THAN( 10, 0 )
