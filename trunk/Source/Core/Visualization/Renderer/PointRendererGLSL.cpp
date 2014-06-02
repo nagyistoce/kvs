f8dbbdd2cb173415f0097511bd18f205e275eb98
naohisa.sakamoto@gmail.com <naohisa.sakamoto@gmail.com@23c4efe2-019b-11de-a295-0b7b0935bf94>
2014-06-02 09:25:07 +0000

    Modified with VBO.

--- trunk/Source/Core/Visualization/Renderer/PointRendererGLSL.cpp
+++ trunk/Source/Core/Visualization/Renderer/PointRendererGLSL.cpp
@@ -14,10 +14,41 @@
 /*****************************************************************************/
 #include "PointRenderer.h"
 #include <kvs/DebugNew>
+#include <kvs/OpenGL>
 #include <kvs/ProgramObject>
 #include <kvs/ShaderSource>
 #include <kvs/VertexShader>
 #include <kvs/FragmentShader>
+
+
+namespace
+{
+
+/*===========================================================================*/
+/**
+ *  @brief  Returns vertex-color array.
+ *  @param  point [in] pointer to the point object
+ */
+/*===========================================================================*/
+kvs::ValueArray<kvs::UInt8> VertexColors( const kvs::PointObject* point )
+{
+    if ( point->numberOfVertices() == point->numberOfColors() ) return point->colors();
+
+    const size_t nvertices = point->numberOfVertices();
+    const kvs::RGBColor color = point->color();
+
+    kvs::ValueArray<kvs::UInt8> colors( nvertices * 3 );
+    for ( size_t i = 0; i < nvertices; i++ )
+    {
+        colors[ 3 * i + 0 ] = color.r();
+        colors[ 3 * i + 1 ] = color.g();
+        colors[ 3 * i + 2 ] = color.b();
+    }
+
+    return colors;
+}
+
+} // end of namespace
 
 
 namespace kvs
@@ -26,286 +57,188 @@ namespace kvs
 namespace glsl
 {
 
+/*===========================================================================*/
+/**
+ *  @brief  Constructs a new PointRenderer class.
+ */
+/*===========================================================================*/
 PointRenderer::PointRenderer():
+    m_width( 0 ),
+    m_height( 0 ),
+    m_object( NULL ),
+    m_has_normal( false ),
     m_shader( NULL )
 {
-    this->setShader( kvs::Shader::Phong() );
+    this->setShader( kvs::Shader::Lambert() );
 }
 
+/*===========================================================================*/
+/**
+ *  @brief  Destructs the PointRenderer class.
+ */
+/*===========================================================================*/
 PointRenderer::~PointRenderer()
 {
     if ( m_shader ) { delete m_shader; }
 }
 
+/*===========================================================================*/
+/**
+ *  @brief  Executes the rendering process.
+ *  @param  object [in] pointer to the object
+ *  @param  camera [in] pointer to the camera
+ *  @param  light [in] pointer to the light
+ */
+/*===========================================================================*/
 void PointRenderer::exec( kvs::ObjectBase* object, kvs::Camera* camera, kvs::Light* light )
 {
-    // Following process is executed once.
-    static bool flag = true;
-    if ( flag )
-    {
-        flag = false;
-        this->initialize_shaders();
-    }
+    kvs::PointObject* point = kvs::PointObject::DownCast( object );
+    m_has_normal = point->numberOfNormals() > 0;
+    if ( !m_has_normal ) setEnabledShading( false );
 
     BaseClass::startTimer();
-    m_phong_shader.bind();
-    BaseClass::exec( object, camera, light );
-    m_phong_shader.unbind();
+    kvs::OpenGL::WithPushedAttrib p( GL_ALL_ATTRIB_BITS );
+
+    const size_t width = camera->windowWidth();
+    const size_t height = camera->windowHeight();
+    const bool window_created = m_width == 0 && m_height == 0;
+    if ( window_created )
+    {
+        m_width = width;
+        m_height = height;
+        m_object = object;
+        this->create_shader_program();
+        this->create_buffer_object( point );
+    }
+
+    const bool window_resized = m_width != width || m_height != height;
+    if ( window_resized )
+    {
+        m_width = width;
+        m_height = height;
+    }
+
+    const bool object_changed = m_object != object;
+    if ( object_changed )
+    {
+        m_object = object;
+        m_shader_program.release();
+        m_vbo.release();
+        this->create_shader_program();
+        this->create_buffer_object( point );
+    }
+
+    kvs::VertexBufferObject::Binder bind1( m_vbo );
+    kvs::ProgramObject::Binder bind2( m_shader_program );
+    {
+        const kvs::Mat4 M = kvs::OpenGL::ModelViewMatrix();
+        const kvs::Mat4 PM = kvs::OpenGL::ProjectionMatrix() * M;
+        const kvs::Mat3 N = kvs::Mat3( M[0].xyz(), M[1].xyz(), M[2].xyz() );
+        m_shader_program.setUniform( "ModelViewMatrix", M );
+        m_shader_program.setUniform( "ModelViewProjectionMatrix", PM );
+        m_shader_program.setUniform( "NormalMatrix", N );
+
+        const size_t nvertices = point->numberOfVertices();
+        const size_t coord_size = nvertices * 3 * sizeof( kvs::Real32 );
+        const size_t color_size = nvertices * 3 * sizeof( kvs::UInt8 );
+
+        KVS_GL_CALL( glPointSize( point->size() ) );
+
+        // Enable coords.
+        KVS_GL_CALL( glEnableClientState( GL_VERTEX_ARRAY ) );
+        KVS_GL_CALL( glVertexPointer( 3, GL_FLOAT, 0, (GLbyte*)NULL + 0 ) );
+
+        // Enable colors.
+        KVS_GL_CALL( glEnableClientState( GL_COLOR_ARRAY ) );
+        KVS_GL_CALL( glColorPointer( 3, GL_UNSIGNED_BYTE, 0, (GLbyte*)NULL + coord_size ) );
+
+        // Enable normals.
+        if ( m_has_normal )
+        {
+            KVS_GL_CALL( glEnableClientState( GL_NORMAL_ARRAY ) );
+            KVS_GL_CALL( glNormalPointer( GL_FLOAT, 0, (GLbyte*)NULL + coord_size + color_size ) );
+        }
+
+        // Draw points.
+        KVS_GL_CALL( glDrawArrays( GL_POINTS, 0, nvertices ) );
+
+        // Disable coords.
+        KVS_GL_CALL( glDisableClientState( GL_VERTEX_ARRAY ) );
+
+        // Disable colors.
+        KVS_GL_CALL( glDisableClientState( GL_COLOR_ARRAY ) );
+
+        // Disable normals.
+        if ( m_has_normal )
+        {
+            KVS_GL_CALL( glDisableClientState( GL_NORMAL_ARRAY ) );
+        }
+    }
+
     BaseClass::stopTimer();
 }
 
-/*==========================================================================*/
+/*===========================================================================*/
 /**
- *  @brief  Initialize OpenGL.
+ *  @brief  Creates shader program.
  */
-/*==========================================================================*/
-void PointRenderer::initialize_shaders( void )
+/*===========================================================================*/
+void PointRenderer::create_shader_program()
 {
-    const std::string vert_code = "shader.vert";
-    const std::string frag_code = "shader.frag";
-
-    kvs::ShaderSource vert( vert_code );
-    kvs::ShaderSource frag( frag_code );
-
-    if ( BaseClass::isEnabledShading() )
+    kvs::ShaderSource vert( "shader.vert" );
+    kvs::ShaderSource frag( "shader.frag" );
+    if ( isEnabledShading() )
     {
         switch ( m_shader->type() )
         {
         case kvs::Shader::LambertShading: frag.define("ENABLE_LAMBERT_SHADING"); break;
         case kvs::Shader::PhongShading: frag.define("ENABLE_PHONG_SHADING"); break;
         case kvs::Shader::BlinnPhongShading: frag.define("ENABLE_BLINN_PHONG_SHADING"); break;
-        default: /* NO SHADING */ break;
+        default: break; /* NO SHADING */
         }
-    }
 
-    m_phong_shader.create( vert, frag );
-
-    m_phong_shader.bind();
-    switch ( m_shader->type() )
-    {
-    case kvs::Shader::LambertShading:
-    {
-        const GLfloat Ka = ((kvs::Shader::Lambert*)(m_shader))->Ka;
-        const GLfloat Kd = ((kvs::Shader::Lambert*)(m_shader))->Kd;
-        m_phong_shader.setUniform( "shading.Ka", Ka );
-        m_phong_shader.setUniform( "shading.Kd", Kd );
-        break;
-    }
-    case kvs::Shader::PhongShading:
-    {
-        const GLfloat Ka = ((kvs::Shader::Phong*)(m_shader))->Ka;
-        const GLfloat Kd = ((kvs::Shader::Phong*)(m_shader))->Kd;
-        const GLfloat Ks = ((kvs::Shader::Phong*)(m_shader))->Ks;
-        const GLfloat S  = ((kvs::Shader::Phong*)(m_shader))->S;
-        m_phong_shader.setUniform( "shading.Ka", Ka );
-        m_phong_shader.setUniform( "shading.Kd", Kd );
-        m_phong_shader.setUniform( "shading.Ks", Ks );
-        m_phong_shader.setUniform( "shading.S",  S );
-        break;
-    }
-    case kvs::Shader::BlinnPhongShading:
-    {
-        const GLfloat Ka = ((kvs::Shader::BlinnPhong*)(m_shader))->Ka;
-        const GLfloat Kd = ((kvs::Shader::BlinnPhong*)(m_shader))->Kd;
-        const GLfloat Ks = ((kvs::Shader::BlinnPhong*)(m_shader))->Ks;
-        const GLfloat S  = ((kvs::Shader::BlinnPhong*)(m_shader))->S;
-        m_phong_shader.setUniform( "shading.Ka", Ka );
-        m_phong_shader.setUniform( "shading.Kd", Kd );
-        m_phong_shader.setUniform( "shading.Ks", Ks );
-        m_phong_shader.setUniform( "shading.S",  S );
-        break;
-    }
-    default: /* NO SHADING */ break;
-    }
-    m_phong_shader.unbind();
-}
-
-/*
-PointRenderer::PointRenderer( void ):
-    m_vertex_buffer( NULL ),
-    m_normal_buffer( NULL ),
-    m_color_buffer( NULL ),
-    m_ref_point( NULL )
-{
-}
-
-PointRenderer::~PointRenderer( void )
-{
-    if ( m_vertex_buffer ) delete m_vertex_buffer;
-    if ( m_normal_buffer ) delete m_normal_buffer;
-    if ( m_color_buffer ) delete m_color_buffer;
-}
-
-void PointRenderer::exec( kvs::ObjectBase* object, kvs::Camera* camera, kvs::Light* light )
-{
-    kvs::IgnoreUnusedVariable( light );
-    kvs::IgnoreUnusedVariable( camera );
-
-    kvs::PointObject* point = kvs::PointObject::DownCast( object );
-
-    glPushAttrib( GL_CURRENT_BIT | GL_ENABLE_BIT );
-
-    if ( point->normals().size() == 0 ) { BaseClass::disableShading(); }
-
-    BaseClass::BaseClass::initialize();
-    point->applyMaterial();
-
-    // Anti-aliasing.
-    if ( m_enable_anti_aliasing )
-    {
-#if defined ( GL_MULTISAMPLE )
-        if ( m_enable_multisample_anti_aliasing )
+        if ( kvs::OpenGL::Boolean( GL_LIGHT_MODEL_TWO_SIDE ) == GL_TRUE )
         {
-            GLint buffers = 0;
-            GLint samples = 0;
-            glGetIntegerv( GL_SAMPLE_BUFFERS, &buffers );
-            glGetIntegerv( GL_SAMPLES, &samples );
-            if ( buffers > 0 && samples > 1 ) glEnable( GL_MULTISAMPLE );
-        }
-        else
-#endif
-        {
-            glEnable( GL_POINT_SMOOTH );
-            glEnable( GL_BLEND );
-            glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
-            glHint( GL_POINT_SMOOTH_HINT, GL_NICEST );
+            frag.define("ENABLE_TWO_SIDE_LIGHTING");
         }
     }
 
-    if ( !m_ref_point || m_ref_point != point )
-    {
-        this->create_vertexbuffer( point );
-        this->download_vertexbuffer( point );
-    }
-
-    glEnable( GL_DEPTH_TEST );
-    {
-        BaseClass::timer().start();
-        this->draw_vertexbuffer( point );
-        BaseClass::timer().stop();
-    }
-    glDisable( GL_DEPTH_TEST );
-
-    glPopAttrib();
+    m_shader_program.build( vert, frag );
+    m_shader_program.bind();
+    m_shader_program.setUniform( "shading.Ka", m_shader->Ka );
+    m_shader_program.setUniform( "shading.Kd", m_shader->Kd );
+    m_shader_program.setUniform( "shading.Ks", m_shader->Ks );
+    m_shader_program.setUniform( "shading.S",  m_shader->S );
+    m_shader_program.unbind();
 }
 
-void PointRenderer::create_vertexbuffer( const kvs::PointObject* point )
+/*===========================================================================*/
+/**
+ *  @brief  Creates buffer object.
+ *  @param  point [in] pointer to the point object
+ */
+/*===========================================================================*/
+void PointRenderer::create_buffer_object( const kvs::PointObject* point )
 {
-    const size_t nnormals = point->normals().size() / 3;
-    const size_t ncolors = point->colors().size() / 3;
+    kvs::ValueArray<kvs::Real32> coords = point->coords();
+    kvs::ValueArray<kvs::UInt8> colors = ::VertexColors( point );
+    kvs::ValueArray<kvs::Real32> normals = point->normals();
 
-    const size_t vertex_size = point->coords().byteSize();
-    const size_t normal_size = point->normals().byteSize();
-    const size_t color_size = point->colors().byteSize();
+    const size_t coord_size = coords.byteSize();
+    const size_t color_size = colors.byteSize();
+    const size_t normal_size = normals.byteSize();
+    const size_t byte_size = coord_size + color_size + normal_size;
 
-    // Vertex.
-    if ( m_vertex_buffer ) delete m_vertex_buffer;
-    m_vertex_buffer = new kvs::glew::VertexBufferObject;
-    m_vertex_buffer->setUsage( kvs::glew::VertexBufferObject::StaticDraw );
-    m_vertex_buffer->create( vertex_size );
-
-    // Normal.
-    if ( m_normal_buffer ) delete m_normal_buffer;
-    m_normal_buffer = new kvs::glew::VertexBufferObject;
-    m_normal_buffer->setUsage( kvs::glew::VertexBufferObject::StaticDraw );
-    if ( nnormals > 0 ) m_normal_buffer->create( normal_size );
-
-    // Color.
-    if ( m_color_buffer ) delete m_color_buffer;
-    m_color_buffer = new kvs::glew::VertexBufferObject;
-    m_color_buffer->setUsage( kvs::glew::VertexBufferObject::StaticDraw );
-    if ( ncolors > 1 ) m_color_buffer->create( color_size );
-
-    const GLenum error = glGetError();
-    if ( error != GL_NO_ERROR )
+    m_vbo.create( byte_size );
+    m_vbo.bind();
+    m_vbo.load( coord_size, coords.data(), 0 );
+    m_vbo.load( color_size, colors.data(), coord_size );
+    if ( normal_size > 0 )
     {
-        kvsMessageError( "Vertex Buffer Object create failed: %s (code: %d).", gluErrorString( error ), error );
+        m_vbo.load( normal_size, normals.data(), coord_size + color_size );
     }
+    m_vbo.unbind();
 }
-
-void PointRenderer::download_vertexbuffer( const kvs::PointObject* point )
-{
-    const size_t nnormals = point->normals().size() / 3;
-    const size_t ncolors = point->colors().size() / 3;
-
-    const size_t vertex_size = point->coords().byteSize();
-    const size_t normal_size = point->normals().byteSize();
-    const size_t color_size = point->colors().byteSize();
-
-    // Vertex.
-    m_vertex_buffer->bind();
-    m_vertex_buffer->download( vertex_size, point->coords().pointer(), 0 );
-    glVertexPointer( 3, GL_FLOAT, 0, 0 );
-
-    // Normal.
-    if ( nnormals > 0 )
-    {
-        m_normal_buffer->bind();
-        m_normal_buffer->download( normal_size, point->normals().pointer(), 0 );
-        glNormalPointer( GL_FLOAT, 0, 0 );
-    }
-
-    // Color.
-    if ( ncolors > 0 )
-    {
-        m_color_buffer->bind();
-        m_color_buffer->download( color_size, point->colors().pointer(), 0 );
-        glColorPointer( 3, GL_UNSIGNED_BYTE, 0, 0 );
-    }
-
-    const GLenum error = glGetError();
-    if ( error != GL_NO_ERROR )
-    {
-        kvsMessageError( "Vertex Buffer Object download failed: %s(%d).", gluErrorString( error ), error );
-    }
-    else
-    {
-        m_ref_point = point;
-    }
-}
-
-void PointRenderer::draw_vertexbuffer( const kvs::PointObject* point )
-{
-    const size_t nvertices = point->coords().size() / 3;
-    const size_t nnormals = point->normals().size() / 3;
-    const size_t ncolors = point->colors().size() / 3;
-
-    // Vertex.
-    m_vertex_buffer->bind();
-    glEnableClientState( GL_VERTEX_ARRAY );
-
-    // Normal.
-    if ( nnormals > 0 )
-    {
-        m_normal_buffer->bind();
-        glEnableClientState( GL_NORMAL_ARRAY );
-    }
-
-    // Color.
-    if ( ncolors > 1 )
-    {
-        m_color_buffer->bind();
-        glEnableClientState( GL_COLOR_ARRAY );
-    }
-    else if ( ncolors == 1 )
-    {
-        const kvs::UInt8* c = point->colors().pointer();
-        glColor3ub( c[0], c[1], c[2] );
-    }
-
-    // Size.
-    {
-        const kvs::Real32 s = point->sizes().at(0);
-        glPointSize( s );
-    }
-
-    glDrawArrays( GL_POINTS, 0, nvertices );
-
-    glDisableClientState( GL_VERTEX_ARRAY );
-    glDisableClientState( GL_NORMAL_ARRAY );
-    glDisableClientState( GL_COLOR_ARRAY );
-}
-*/
 
 } // end of namespace glsl
 
